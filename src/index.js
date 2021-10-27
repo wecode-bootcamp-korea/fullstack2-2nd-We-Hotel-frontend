56a135374f280341a400cba78e0630ead8bfda24
goplanit19@gmail.com <goplanit19@gmail.com>
2021-10-27 22:06:49 +0900

    add: msw를 이용하여 메인슬라이드 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import initMockAPI from './mocks';
 import Router from './Routes';
 import GlobalStyle from './styles/GlobalStyle';
+
+if (process.env.NODE_ENV === 'development') {
+  initMockAPI();
+}
 
 ReactDOM.render(
   <>
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Carousel from '../../components/Carousel/Carousel';
 import styled from 'styled-components';
 
 function Main() {
-  return <Container>Main Page</Container>;
+  const [carouselItem, setCarouselItem] = useState([]);
+
+  const loadCarouselItem = async () => {
+    try {
+      let response = await axios.get('/carousel');
+      setCarouselItem(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadCarouselItem();
+  }, []);
+
+  return (
+    <Container>
+      <Carousel
+        carouselItem={carouselItem}
+        type="main"
+        width="720"
+        height="360"
+      />
+    </Container>
+  );
 }
 
 export default Main;
 
 const Container = styled.div`
-  background-color: blue;
+  margin: 0 auto;
+  overflow: hidden;
 `;
