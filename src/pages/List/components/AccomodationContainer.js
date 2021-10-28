e237419302e4d13314dc9e4f248f74ccf788ab78
윤창현 <changh950@gmail.com>
2021-10-28 12:47:43 +0900

    mydaily 레이아웃 작업

---
+++ src/pages/List/components/AccomodationContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function AccommodationContainer() {
+  return <AccommodationBar>1</AccommodationBar>;
+}
+
+export default AccommodationContainer;
+
+const AccommodationBar = styled.main`
+  width: 768px;
+  height: 36px;
+  background-color: darkblue;
+`;
