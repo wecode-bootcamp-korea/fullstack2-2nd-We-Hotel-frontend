20dee4c39b066eb70fc8f5c028bdd2a3b726c51b
goplanit19@gmail.com <goplanit19@gmail.com>
2021-10-28 11:57:53 +0900

    fix: 화면 렌더링이 늦어지는 오류 수정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import initMockAPI from './mocks';
 import Router from './Routes';
 import GlobalStyle from './styles/GlobalStyle';
-
-if (process.env.NODE_ENV === 'development') {
-  initMockAPI();
-}
 
 ReactDOM.render(
   <>
