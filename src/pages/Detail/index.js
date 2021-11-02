ba91cab5529bab7c066a672ca775c7dc9acdb1ea
goplanit19@gmail.com <goplanit19@gmail.com>
2021-11-03 08:19:36 +0900

    네이버 맵 API를 이용한 지도 구현

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,3 +1,38 @@
-import DetailContainer from './DetailContainer';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TrueReview from './TrueReview';
+import Map from '../../components/Map/Map';
+import styled from 'styled-components';
 
-export default DetailContainer;
+function Detail() {
+  const [mapItem, setmapItem] = useState([]);
+
+  useEffect(() => {
+    const loadItem = async () => {
+      try {
+        let resMapItem = await axios.get('/detail');
+        setmapItem(resMapItem.data[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    loadItem();
+  }, []);
+
+  return (
+    <Container>
+      <TrueReview trueReview />
+      <Map mapItem={mapItem} width="768" />
+    </Container>
+  );
+}
+
+export default Detail;
+
+const Container = styled.div`
+  position: relative;
+  width: 768px;
+  height: 100vh;
+  margin: 0 auto;
+`;
