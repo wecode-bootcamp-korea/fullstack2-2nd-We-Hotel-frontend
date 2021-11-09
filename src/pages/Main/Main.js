d52603b0cb9abb15bf2a3d607c2b6e7ea19777aa
goplanit19@gmail.com <goplanit19@gmail.com>
2021-11-09 13:52:35 +0900

    refector: fetch 데이터 promise.all로 병렬처리, mockData 수정, history 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 import Carousel from '../../components/Carousel/Carousel';
 import Header from '../../components/Header/Header';
@@ -8,20 +7,36 @@ import Footer from '../../components/Footer/Footer.js';
 import Grid from '../../components/Grid/Grid';
 import styled from 'styled-components';
 
-function Main({ history }) {
+function Main() {
   const [carouselItem, setCarouselItem] = useState([]);
   const [locationItem, setLocationItem] = useState([]);
   const [categoryItem, setCategoryItem] = useState([]);
+  const URLs = ['/carousel', '/locations', '/category'];
+  const getAllData = URLs => {
+    return Promise.all(URLs.map(fetchData));
+  };
+
+  const fetchData = URL => {
+    return axios
+      .get(URL)
+      .then(function (res) {
+        return { success: true, data: res.data };
+      })
+      .catch(function (err) {
+        return {
+          success: false,
+        };
+      });
+  };
 
   useEffect(() => {
-    const loadItem = async () => {
+    const loadItem = () => {
       try {
-        let resCarousel = await axios.get('/carousel');
-        let resLocations = await axios.get('/locations');
-        let resCategory = await axios.get('/category');
-        setCarouselItem(resCarousel.data);
-        setLocationItem(resLocations.data);
-        setCategoryItem(resCategory.data);
+        getAllData(URLs).then(res => {
+          setCarouselItem(res[0].data);
+          setLocationItem(res[1].data);
+          setCategoryItem(res[2].data);
+        });
       } catch (error) {
         console.log(error);
       }
@@ -39,21 +54,13 @@ function Main({ history }) {
         width="720"
         height="360"
       />
-      <Category
-        categoryItem={categoryItem}
-        _onClick={() => {
-          history.push(`/list/1`);
-        }}
-      />
+      <Category categoryItem={categoryItem} />
       <Grid
         locationItem={locationItem.seoul}
         width="720"
         type="grid4x4"
         title="고민타파! WeHotel 최고 인기의 서울 호텔 Best4"
         subTitle="한주간 예약율이 가장 높은 Weekly Choice!"
-        _onClick={() => {
-          history.push(`/list/2`);
-        }}
       />
       <Grid
         locationItem={locationItem.pusan}
@@ -61,9 +68,6 @@ function Main({ history }) {
         type="grid4x4"
         title="오늘만 특별할인! 부산으로 떠날 절호의 기회!"
         subTitle="부산의 낭만을 선사해줄 WeHotel만의 특별한 선택!"
-        _onClick={() => {
-          history.push(`/list/3`);
-        }}
       />
       <Grid
         locationItem={locationItem.jeju}
@@ -71,9 +75,6 @@ function Main({ history }) {
         type="grid4x4"
         title="신혼여행객들을 위한 WeHotel의 깜짝 이벤트~!"
         subTitle="인생 최고의 경험을 위해 특별한 호텔을 준비했어요~!"
-        _onClick={() => {
-          history.push(`/list/4`);
-        }}
       />
       <Grid
         locationItem={locationItem.gangwon}
@@ -81,16 +82,13 @@ function Main({ history }) {
         type="grid4x4"
         title="멀리가지마세요~ 강원가세요~!"
         subTitle="볼거리와 먹을거리 둘다 놓치지 않기 위한 스페셜강원!"
-        _onClick={() => {
-          history.push(`/location/gangwon`);
-        }}
       />
       <Footer />
     </Container>
   );
 }
 
-export default withRouter(Main);
+export default Main;
 
 const Container = styled.div`
   margin: 0 auto;
